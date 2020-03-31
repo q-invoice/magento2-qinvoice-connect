af3a39998187cdec130afbe95d2d6c10f86b0347
Alex Radchenko <olexandr.radchenko@gmail.com>
2020-03-31 17:23:23 +0300

    Fix config issue Change token generation to random logic Move token generation to datapatch

--- Block/System/Config/Form/Field/Webshopsecret.php
+++ Block/System/Config/Form/Field/Webshopsecret.php
@@ -2,33 +2,14 @@
 namespace Qinvoice\Connect\Block\System\Config\Form\Field;
 
 class Webshopsecret extends \Magento\Config\Block\System\Config\Form\Field
-{   
-	protected $_storeManager;
-	
-	public function __construct
-	(
-		\Magento\Store\Model\StoreManagerInterface $storeManager,
-		\Magento\Framework\App\Config\ConfigResource\ConfigInterface  $resourceConfig
-	)
-	{
-		$this->_storeManager = $storeManager;
-		$this->resourceConfig = $resourceConfig;
-	}
-	
-    protected function _getElementHtml(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+{
+    /**
+     * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
+     * @return string
+     */
+    public function render(\Magento\Framework\Data\Form\Element\AbstractElement $element)
     {
-		 if ($element->getValue() == '') {
-        	$value = sha1( $this->_storeManager->getStore()->getName() . $this->_storeManager->getStore()->getBaseUrl() );
-            $element->setValues( array('value' => $value ));
-            $this->resourceConfig->saveConfig(
-				'invoice_options/invoice/webshop_secret',
-				$value,
-				\Magento\Framework\App\Config\ScopeConfigInterface::SCOPE_TYPE_DEFAULT,
-				\Magento\Store\Model\Store::DEFAULT_STORE_ID
-			);
-        }
-        $element->setReadonly('readonly');
-		
-        return parent::_getElementHtml($element);
-    }    
+        $element->setReadonly(1);
+        return parent::render($element);
+    }
 }
