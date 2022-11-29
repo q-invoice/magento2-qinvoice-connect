00cd5b5a9092c73d4f5555c6946c41300cea563c
Casper <casper@Caspers-MacBook-Pro.local>
2022-11-29 14:15:26 +0100

    array

--- Setup/Patch/Data/CreateIntegrationUser.php
+++ Setup/Patch/Data/CreateIntegrationUser.php
@@ -60,7 +60,7 @@ class CreateIntegrationUser implements DataPatchInterface
         $this->moduleDataSetup->getConnection()->startSetup();
 
         try{
-            $this->configBasedIntegrationManager->processConfigBasedIntegrations([self::Q_INVOICE_INTEGRATION_NAME => 1]);
+            $this->configBasedIntegrationManager->processConfigBasedIntegrations([self::Q_INVOICE_INTEGRATION_NAME => array('resource' => 1)]);
         
             $integration = $this->integrationService->findByName(self::Q_INVOICE_INTEGRATION_NAME);
             $consumerId = $integration->getConsumerId();
