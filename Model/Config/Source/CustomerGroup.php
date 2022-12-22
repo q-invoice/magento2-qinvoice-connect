0659ef1980e89d34067b84ea68eb46e564954422
valdasbuk <ValdasBukauskas@proton.me>
2022-12-22 15:32:18 +0100

    group filter

--- Model/Config/Source/CustomerGroup.php
+++ Model/Config/Source/CustomerGroup.php
@@ -6,7 +6,11 @@
 namespace Qinvoice\Connect\Model\Config\Source;
 
 use Magento\Customer\Api\GroupRepositoryInterface;
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Framework\Api\SearchCriteria;
 use Magento\Framework\Data\OptionSourceInterface;
+use \Magento\Framework\Api\FilterBuilder;
+use \Magento\Framework\Api\Search\FilterGroupBuilder;
 
 class CustomerGroup implements OptionSourceInterface
 {
@@ -14,14 +18,29 @@ class CustomerGroup implements OptionSourceInterface
      * @var GroupRepositoryInterface
      */
     protected $groupRepository;
+    /**
+     * @var SearchCriteriaBuilder
+     */
+    private SearchCriteriaBuilder $searchCriteriaBuilder;
+    private \Magento\Framework\Api\FilterBuilder $filterBuilder;
+    private \Magento\Framework\Api\Search\FilterGroupBuilder $filterGroupBuilder;
+
 
     /**
      * @param GroupRepositoryInterface $groupRepository
+     * @param SearchCriteriaBuilder $searchCriteriaBuilder
      */
     public function __construct(
-        GroupRepositoryInterface $groupRepository
-    ) {
+        GroupRepositoryInterface $groupRepository,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        FilterBuilder $filterBuilder,
+        FilterGroupBuilder $filterGroupBuilder
+    )
+    {
         $this->groupRepository = $groupRepository;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->filterBuilder = $filterBuilder;
+        $this->filterGroupBuilder = $filterGroupBuilder;
     }
 
     /**
@@ -29,7 +48,27 @@ class CustomerGroup implements OptionSourceInterface
      */
     public function toOptionArray()
     {
-        $customerGroups = $this->groupRepository->getList(\Magento\Customer\Model\Group::CUST_GROUP_ALL);
+
+
+        // Create a SearchCriteriaBuilder object
+        $searchCriteriaBuilder = $this->searchCriteriaBuilder->create();
+
+
+        $filter_1 = $this->filterBuilder
+            ->setField('customer_group_id')
+            ->setConditionType('neq')
+            ->setValue(\Magento\Customer\Model\Group::CUST_GROUP_ALL)
+            ->create();
+
+        $filter_group = $this->filterGroupBuilder
+            ->addFilter($filter_1)
+            ->create();
+
+        // Set the filters for the search criteria
+        $searchCriteriaBuilder->setFilterGroups($filter_group);
+
+
+        $customerGroups = $this->groupRepository->getList(SearchCriteria::PAGE_SIZE);
         $options = [];
         foreach ($customerGroups as $customerGroup) {
             $options[] = [
