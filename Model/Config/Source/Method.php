b21d0518523e260568c7352ebec530b228e62819
valdasbuk <ValdasBukauskas@proton.me>
2022-12-22 14:36:16 +0100

    order + source

--- Model/Config/Source/Method.php
+++ Model/Config/Source/Method.php
@@ -12,10 +12,10 @@ class Method implements ArrayInterface
     public function toOptionArray()
     {
         return [
-            ['value' => 'dynamic', 'label' => __('Dynamic, with VAT for consumers, without for companies')],
+            ['value' => 'automatic', 'label' => __('Automatic, with VAT for consumers, without for companies')],
             ['value' => 'excl', 'label' => __('Prices without VAT are leading')],
             ['value' => 'incl', 'label' => __('Prices with VAT included are leading')],
-            ['value' => 'customer_groups', 'label' => __('Define separate rules for customer groups')],
+            ['value' => 'customer_groups', 'label' => __('Define separate rules for customer groups')]
         ];
     }
 }
